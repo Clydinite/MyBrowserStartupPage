3e800f8ed5851facc6220d913488acc138fd7322
Clydinite <clydinite@gmail.com>
2024-03-10 19:07:21 +0800

    icons with add link button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Icon from './Icon.svelte';
+
+	class Link {
+		title: string;
+		href: string;
+
+		constructor(title: string, href: string) {
+			this.title = title;
+			this.href = href;
+		}
+	}
+
+	const links: Link[] = [
+		new Link('GitHub', 'https://github.com/'),
+		new Link('Twitter', 'https://twitter.com/'),
+		new Link('Discord', 'https://discord.com/'),
+		new Link('YouTube', 'https://youtube.com/')
+	];
+</script>
+
+<div class="h-full w-full bg-slate-950">
+	<div class="h-full w-full p-20">
+		<div class="grid h-full w-full grid-cols-4 place-items-center gap-3 md:grid-cols-6">
+			{#each links as { title, href }}
+				<Icon {title} {href}></Icon>
+			{/each}
+            <Icon title='' href='' isPlus={true}></Icon>
+		</div>
+	</div>
+</div>
---
+++ src/routes/Icon.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	export let title: string;
+	export let href: string;
+	export let isPlus: boolean = false;
+</script>
+
+<div>
+	{#if isPlus}
+		<div class="h-12 w-12 md:h-16 md:w-16">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="rounded-md bg-slate-50/20 stroke-slate-200 p-2"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+			</svg>
+		</div>
+		<p class="py-2 text-center text-white">New Link</p>
+	{:else}
+		<a {href} target="_blank">
+			<div>
+				<img
+					src="https://www.google.com/s2/favicons?sz=64&domain_url={href}"
+					alt="{title} logo"
+					class="h-12 w-12 rounded-md object-cover md:h-16 md:w-16"
+				/>
+			</div>
+			<p class="py-2 text-center text-white">{title}</p>
+		</a>
+	{/if}
+</div>
