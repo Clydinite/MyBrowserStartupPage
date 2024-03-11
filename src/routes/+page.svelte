27e2a4d4e7c44972a11a3250b0c90a362fee7191
Clydinite <clydinite@gmail.com>
2024-03-11 23:21:31 +0800

    added object-cover to make sure that the icons fit inside their container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -136,7 +136,7 @@
 								viewBox="0 0 24 24"
 								stroke-width="1.5"
 								stroke="currentColor"
-								class="rounded-md bg-slate-50/20 stroke-slate-200 p-3"
+								class="object-cover rounded-md bg-slate-50/20 stroke-slate-200 p-3"
 							>
 								<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
 							</svg>
